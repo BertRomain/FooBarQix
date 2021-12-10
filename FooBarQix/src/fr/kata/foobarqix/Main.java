a22f3dd01d96bd51763d3b65fd1225e75bc9fdff
Romain B <r.bertoux@free.fr>
2021-12-10 10:30:25 +0100

    ajout/modification de tests + modifications de l'algo pour l'étape 2, ajout du main

--- FooBarQix/src/fr/kata/foobarqix/Main.java
+++ FooBarQix/src/fr/kata/foobarqix/Main.java
@@ -3,7 +3,10 @@ package fr.kata.foobarqix;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Integer [] myArray  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 21, 33, 51, 53, 101, 303, 105, 10101} ;
+		
+		FooBarQix.convertToFooBarQix(myArray);
 
 	}
 
